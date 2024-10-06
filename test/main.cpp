9f4fcdd052ecaa5e766b37c6ed2d5947cefce1e1
giosali <70548443+giosali@users.noreply.github.com>
2024-10-05 20:06:21 -0500

    Delete SwipeAction and Swipe

--- test/main.cpp
+++ test/main.cpp
@@ -2,7 +2,6 @@
 #include "gesturesfile_test.h"
 #include "inputinjector_test.h"
 #include "paths_test.h"
-#include "swipeaction_test.h"
 #include "swipeparser_test.h"
 #include "ut.hpp"
 #include "utils_test.h"
