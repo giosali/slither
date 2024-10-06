3e38c40b58210dc3daa3dd9285e5fe0a38812df2
giosali <70548443+giosali@users.noreply.github.com>
2024-10-05 19:41:46 -0500

    Move Watch login into detached thread

--- src/gesturesfile.cpp
+++ src/gesturesfile.cpp
@@ -4,6 +4,7 @@
 
 #include <fstream>
 #include <iostream>
+#include <thread>
 
 #include "json.hpp"
 
@@ -25,41 +26,47 @@ GesturesFile::GesturesFile(const std::filesystem::path& path) : path_{path} {
 std::vector<Gesture> GesturesFile::GetGestures() const { return gestures_; }
 
 void GesturesFile::Watch() {
-  auto fd = inotify_init();
-  if (fd < 0) {
-    std::cerr << "Error initializing inotify: " << strerror(errno) << "\n";
-    return;
-  }
-
-  auto wd = inotify_add_watch(fd, path_.c_str(), IN_MODIFY);
-  if (wd < 0) {
-    std::cerr << "Error adding watch: " << strerror(errno) << "\n";
-    close(fd);
-  }
-
-  auto event_size = sizeof(struct inotify_event);
-  auto buffer_length = 1024 * (event_size + 16);
-  char buffer[buffer_length];
-  while (true) {
-    auto length = read(fd, buffer, buffer_length);
-    if (length == -1 && errno != EAGAIN) {
-      std::cerr << "Error reading events: " << strerror(errno) << "\n";
-      break;
+  auto t = std::thread{[this]() {
+    auto fd = inotify_init();
+    if (fd < 0) {
+      std::cerr << "Error initializing inotify: " << strerror(errno) << "\n";
+      return;
     }
 
-    auto i = 0;
-    while (i < length) {
-      auto event = (struct inotify_event*)&buffer[i];
-      if (event->mask & IN_MODIFY) {
-        UpdateGestures();
+    auto wd = inotify_add_watch(fd, path_.c_str(), IN_MODIFY);
+    if (wd < 0) {
+      std::cerr << "Error adding watch: " << strerror(errno) << "\n";
+      close(fd);
+    }
+
+    auto event_size = sizeof(struct inotify_event);
+    auto buffer_length = 1024 * (event_size + 16);
+    char buffer[buffer_length];
+    while (true) {
+      auto length = read(fd, buffer, buffer_length);
+      if (length == -1 && errno != EAGAIN) {
+        std::cerr << "Error reading events: " << strerror(errno) << "\n";
+        break;
       }
 
-      i += event_size + event->len;
-    }
-  }
+      auto i = 0;
+      while (i < length) {
+        auto event = (struct inotify_event*)&buffer[i];
+        if (event->mask & IN_MODIFY) {
+          UpdateGestures();
+          for (const auto& g : gestures_) {
+            std::cout << g.GetFingerCount() << "\n";
+          }
+        }
 
-  inotify_rm_watch(fd, wd);
-  close(fd);
+        i += event_size + event->len;
+      }
+    }
+
+    inotify_rm_watch(fd, wd);
+    close(fd);
+  }};
+  t.detach();
 }
 
 void GesturesFile::UpdateGestures() {
