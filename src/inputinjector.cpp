e43dfa6b7d1fe058774fb716c5f73351fa813c35
giosali <70548443+giosali@users.noreply.github.com>
2024-10-07 15:31:16 -0500

    Convert InputInjector class into a class with a single static function Inject

--- src/inputinjector.cpp
+++ src/inputinjector.cpp
@@ -1,86 +1,49 @@
 #include "inputinjector.h"
 
-#include <list>
-#include <stdexcept>
+#include <libevdev/libevdev-uinput.h>
+#include <libevdev/libevdev.h>
 
-#include "utils.h"
-
-InputInjector::InputInjector() : dev_{nullptr}, uinput_dev_{nullptr} {}
-
-InputInjector::InputInjector(const std::unordered_set<uint32_t>& key_codes)
-    : dev_{libevdev_new()}, uinput_dev_{nullptr} {
-  libevdev_set_name(dev_, "slither device");
-
-  auto err = libevdev_enable_event_type(dev_, EV_KEY);
-  if (err != 0) {
-    auto what = Utils::FormatExceptionMessage(
-      "Enabling event type on new libevdev device",
-      "Unable to enable EV_KEY event");
-    throw std::runtime_error{what};
-  }
-
-  // Enables key codes to be programmatically pressed/released.
-  for (auto key_code : key_codes) {
-    libevdev_enable_event_code(dev_, EV_KEY, key_code, nullptr);
-  }
-
-  err = libevdev_uinput_create_from_device(dev_, LIBEVDEV_UINPUT_OPEN_MANAGED,
-                                           &uinput_dev_);
-  if (err != 0) {
-    auto what = Utils::FormatExceptionMessage(
-      "Creating a uinput device from the newly created libevdev device",
-      "The program does not have permission to open /dev/uinput in read/write "
-      "mode",
-      "The program must either be run with 'sudo' or the user must be a member "
-      "of the input group");
-    throw std::runtime_error{what};
-  }
-}
-
-InputInjector::~InputInjector() {
-  // The uinput device must be destroyed before freeing the libevdev device.
-  libevdev_uinput_destroy(uinput_dev_);
-  libevdev_free(dev_);
-}
+#include <chrono>
+#include <thread>
 
 void InputInjector::Inject(const std::vector<uint32_t>& key_codes) {
-  // TODO: rework this entire block of logic, This can be made much more
-  // efficient.
-
-  auto modifier_key_codes = std::list<uint32_t>{};
+  auto dev = libevdev_new();
+  libevdev_set_name(dev, "Slither Device");
+  libevdev_enable_event_type(dev, EV_KEY);
 
   for (size_t i = 0; i < key_codes.size(); ++i) {
-    auto key_code = key_codes[i];
-    switch (key_code) {
-      case KEY_LEFTCTRL:
-      case KEY_RIGHTCTRL:
-      case KEY_LEFTSHIFT:
-      case KEY_RIGHTSHIFT:
-      case KEY_LEFTALT:
-      case KEY_RIGHTALT:
-      case KEY_LEFTMETA:
-      case KEY_RIGHTMETA:
-        PressKey(key_code);
-        modifier_key_codes.push_front(key_code);
-        break;
-      default:
-        PressKey(key_code);
-        ReleaseKey(key_code);
-        break;
-    }
+    libevdev_enable_event_code(dev, EV_KEY, key_codes[i], nullptr);
   }
 
-  for (auto modifier_key_code : modifier_key_codes) {
-    ReleaseKey(modifier_key_code);
+  libevdev_uinput* uinput_dev = nullptr;
+  libevdev_uinput_create_from_device(dev, LIBEVDEV_UINPUT_OPEN_MANAGED,
+                                     &uinput_dev);
+
+  // This time delay is necessary to give enough time for the uinput device to
+  // be created.
+  std::this_thread::sleep_for(std::chrono::milliseconds(50));
+
+  // Simulates key presses.
+  for (size_t i = 0; i < key_codes.size(); ++i) {
+    libevdev_uinput_write_event(uinput_dev, EV_KEY, key_codes[i], 1);
   }
-}
 
-void InputInjector::PressKey(uint32_t key_code) {
-  libevdev_uinput_write_event(uinput_dev_, EV_KEY, key_code, 1);
-  libevdev_uinput_write_event(uinput_dev_, EV_SYN, SYN_REPORT, 0);
-}
+  // Syncs keyboard events.
+  libevdev_uinput_write_event(uinput_dev, EV_SYN, SYN_REPORT, 0);
 
-void InputInjector::ReleaseKey(uint32_t key_code) {
-  libevdev_uinput_write_event(uinput_dev_, EV_KEY, key_code, 0);
-  libevdev_uinput_write_event(uinput_dev_, EV_SYN, SYN_REPORT, 0);
+  // This time delay is necessary to simulate the average duration between key
+  // presses and key releases.
+  std::this_thread::sleep_for(std::chrono::milliseconds(50));
+
+  // Simulates key releases.
+  for (int32_t i = key_codes.size() - 1; i >= 0; --i) {
+    libevdev_uinput_write_event(uinput_dev, EV_KEY, key_codes[i], 0);
+  }
+
+  // Syncs keyboard events.
+  libevdev_uinput_write_event(uinput_dev, EV_SYN, SYN_REPORT, 0);
+
+  // The uinput device must be destroyed before freeing the libevdev device.
+  libevdev_uinput_destroy(uinput_dev);
+  libevdev_free(dev);
 }
--- src/inputinjector.h
+++ src/inputinjector.h
@@ -1,28 +1,14 @@
 #ifndef INPUTINJECTOR_H
 #define INPUTINJECTOR_H
 
-#include <libevdev/libevdev-uinput.h>
-#include <libevdev/libevdev.h>
-
 #include <cstdint>
-#include <unordered_set>
 #include <vector>
 
 class InputInjector {
  public:
-  InputInjector();
-  explicit InputInjector(const std::unordered_set<uint32_t>& key_codes);
+  InputInjector() = delete;
 
-  ~InputInjector();
-
-  void Inject(const std::vector<uint32_t>& key_codes);
-
- private:
-  void PressKey(uint32_t key_code);
-  void ReleaseKey(uint32_t key_code);
-
-  libevdev* dev_;
-  libevdev_uinput* uinput_dev_;
+  static void Inject(const std::vector<uint32_t>& key_codes);
 };
 
 #endif  // INPUTINJECTOR_H
