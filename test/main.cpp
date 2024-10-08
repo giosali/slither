17f0b2106fe615d0f0542f1c72fc3b9167ee7f10
giosali <70548443+giosali@users.noreply.github.com>
2024-10-08 17:20:09 -0500

    Add tests for PinchParser

--- test/main.cpp
+++ test/main.cpp
@@ -2,6 +2,7 @@
 #include "gesturesfile_test.h"
 #include "inputinjector_test.h"
 #include "paths_test.h"
+#include "pinchparser_test.h"
 #include "swipeparser_test.h"
 #include "ut.hpp"
 #include "utils_test.h"
---
+++ test/pinchparser_test.h
@@ -0,0 +1,102 @@
+#ifndef PINCHPARSER_TEST_H
+#define PINCHPARSER_TEST_H
+
+#include "../src/pinchparser.h"
+#include "ut.hpp"
+
+boost::ut::suite<"pinchparser"> pinchparser = [] {
+  using namespace boost::ut;
+  using namespace boost::ut::bdd;
+  using namespace boost::ut::literals;
+
+  "GetDirection"_test = [] {
+    given("The user pinches in various directions") = [] {
+      then("I expect the pinches to match the evaluated directions") =
+        [](const std::tuple<double, Gesture::Direction>& data) {
+          auto dx = std::get<0>(data);
+          auto direction = std::get<1>(data);
+
+          auto parser = PinchParser{};
+          parser.Begin();
+          parser.Update(dx, 0);
+          parser.End(1);
+
+          expect(parser.GetDirection() == direction);
+        } |
+        std::vector<std::tuple<double, Gesture::Direction>>{
+          {0, Gesture::Direction::kNone},   {20, Gesture::Direction::kNone},
+          {-19, Gesture::Direction::kNone}, {39, Gesture::Direction::kNone},
+          {40, Gesture::Direction::kIn},    {-20, Gesture::Direction::kOut}};
+    };
+  };
+
+  "IsGestureValid"_test = [] {
+    given("The user pinches inward by 40 dpi") = [] {
+      then("I expect the gesture to be valid") = [] {
+        auto parser = PinchParser{};
+        parser.Begin();
+        parser.Update(40, 0);
+        parser.End(1);
+
+        expect(parser.IsGestureValid() == true);
+      };
+    };
+
+    given("The user pinches inward by 20 dpi") = [] {
+      then("I expect the gesture to be invalid") = [] {
+        auto parser = PinchParser{};
+        parser.Begin();
+        parser.Update(20, 0);
+        parser.End(1);
+
+        expect(parser.IsGestureValid() == false);
+      };
+    };
+
+    given("The user pinches outward by 20 dpi") = [] {
+      then("I expect the gesture to be valid") = [] {
+        auto parser = PinchParser{};
+        parser.Begin();
+        parser.Update(-20, 0);
+        parser.End(1);
+
+        expect(parser.IsGestureValid() == true);
+      };
+    };
+
+    given("The user pinches outward by 10 dpi") = [] {
+      then("I expect the gesture to be invalid") = [] {
+        auto parser = PinchParser{};
+        parser.Begin();
+        parser.Update(-10, 0);
+        parser.End(1);
+
+        expect(parser.IsGestureValid() == false);
+      };
+    };
+
+    given("The user ends their pinch gesture on time") = [] {
+      then("I expect the gesture to be valid") = [](uint32_t time) {
+        auto parser = PinchParser{};
+        parser.Begin();
+        parser.Update(40, 0);
+        parser.End(time);
+
+        expect(parser.IsGestureValid() == true);
+      } | std::vector{0, 1, 50, 100, 150, 200, 250};
+    };
+
+    given("The user ends their pinch gesture too late") = [] {
+      then("I expect the gesture to be invalid") = [](uint32_t time) {
+        auto parser = PinchParser{};
+        parser.Begin();
+        parser.Update(40, 0);
+        parser.End(time);
+
+        expect(parser.IsGestureValid() == false);
+      } | std::vector{251, 300, 400, 500, 1000};
+    };
+  };
+};
+
+#endif  // PINCHPARSER_TEST_H
