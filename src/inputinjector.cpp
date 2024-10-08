1405e78ea470370adb7b41f550ef7338e433a72f
giosali <70548443+giosali@users.noreply.github.com>
2024-10-08 09:02:34 -0500

    Add time delay after keyboard input is injected

--- src/inputinjector.cpp
+++ src/inputinjector.cpp
@@ -43,6 +43,10 @@ void InputInjector::Inject(const std::vector<uint32_t>& key_codes) {
   // Syncs keyboard events.
   libevdev_uinput_write_event(uinput_dev, EV_SYN, SYN_REPORT, 0);
 
+  // This time delay seems to be necessary to allow for pinch gestures to work
+  // for some reason.
+  std::this_thread::sleep_for(std::chrono::milliseconds(50));
+
   // The uinput device must be destroyed before freeing the libevdev device.
   libevdev_uinput_destroy(uinput_dev);
   libevdev_free(dev);
