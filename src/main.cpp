eeb75f09e8ec40a32d063583e9f5cd87a627dceb
giosali <70548443+giosali@users.noreply.github.com>
2024-10-09 20:36:50 -0500

    Parse command-line arguments and enable spdlog logging when --verbose is passed

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,32 @@
+#include <spdlog/spdlog.h>
+
+#include <iostream>
+
+#include "argparse.hpp"
 #include "gesturewatcher.h"
 
-int main() {
+int main(int argc, char* argv[]) {
+  auto program = argparse::ArgumentParser{"Slither"};
+
+  program.add_argument("--verbose")
+    .help("Print various debugging information")
+    .default_value(false)
+    .implicit_value(true);
+
+  // Basic, just-in-case error handling for argument parsing.
+  try {
+    program.parse_args(argc, argv);
+  } catch (const std::exception& e) {
+    std::cerr << e.what() << "\n";
+    std::cerr << program;
+    return 1;
+  }
+
+  auto verbose = program.get<bool>("--verbose");
+  if (verbose) {
+    spdlog::set_level(spdlog::level::debug);
+  }
+
   auto gesture_watcher = GestureWatcher{};
   gesture_watcher.Enable();
 }
