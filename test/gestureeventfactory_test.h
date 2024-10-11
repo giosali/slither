f4ee29bdb13b18505a583f9fa60d7d0442fecb1f
giosali <70548443+giosali@users.noreply.github.com>
2024-10-11 15:15:22 -0500

    Add test for GestureEventFactory

---
+++ test/gestureeventfactory_test.h
@@ -0,0 +1,32 @@
+#ifndef GESTUREEVENTFACTORY_TEST_H
+#define GESTUREEVENTFACTORY_TEST_H
+
+#include <libinput.h>
+
+#include <vector>
+
+#include "../src/gestureeventfactory.h"
+#include "ut.hpp"
+
+boost::ut::suite<"gestureeventfactory"> gestureeventfactory = [] {
+  using namespace boost::ut;
+  using namespace boost::ut::bdd;
+  using namespace boost::ut::literals;
+
+  "CreateGestureEvent"_test = [] {
+    given("I pass an invalid gesture event type") = [] {
+      then("I expect an exception to be thrown") =
+        [](libinput_event_type event_type) {
+          expect(throws([event_type] {
+            GestureEventFactory::CreateGestureEvent(nullptr, event_type);
+          }));
+        } |
+        std::vector<libinput_event_type>{
+          LIBINPUT_EVENT_GESTURE_SWIPE_UPDATE, LIBINPUT_EVENT_GESTURE_SWIPE_END,
+          LIBINPUT_EVENT_GESTURE_PINCH_UPDATE, LIBINPUT_EVENT_GESTURE_PINCH_END,
+          LIBINPUT_EVENT_GESTURE_HOLD_END};
+    };
+  };
+};
+
+#endif  // GESTUREEVENTFACTORY_TEST_H
