acd60f8a8adeca5a5bceb981297d0b1e5d57ff26
giosali <70548443+giosali@users.noreply.github.com>
2024-10-11 16:58:18 -0500

    Add logging statements

--- src/swipegestureevent.cpp
+++ src/swipegestureevent.cpp
@@ -1,5 +1,7 @@
 #include "swipegestureevent.h"
 
+#include <spdlog/spdlog.h>
+
 #include "gesturesfile.h"
 #include "inputinjector.h"
 
@@ -7,17 +9,30 @@ SwipeGestureEvent::SwipeGestureEvent(libinput_event* event)
     : GestureEvent{event} {}
 
 void SwipeGestureEvent::Begin(libinput_event* event) {
+  spdlog::info("Entering SwipeGestureEvent::Begin");
+
   auto gesture_event = libinput_event_get_gesture_event(event);
   auto time = libinput_event_gesture_get_time(gesture_event);
+  spdlog::debug("Value of time: {}", time);
 
   time_ = time;
+
+  spdlog::info("Exiting SwipeGestureEvent::Begin");
 }
 
 void SwipeGestureEvent::End(libinput_event* event) {
+  spdlog::info("Entering SwipeGestureEvent::End");
+
   auto gesture_event = libinput_event_get_gesture_event(event);
   auto time = libinput_event_gesture_get_time(gesture_event);
+  spdlog::debug("Value of time: {}", time);
 
-  if (direction_ == Gesture::Direction::kNone || time - time_ > kTimeLimit) {
+  auto time_diff = time - time_;
+  if (direction_ == Gesture::Direction::kNone || time_diff > kTimeLimit) {
+    spdlog::info("direction_ or time difference is invalid");
+    spdlog::debug("Value of direction_: {}", static_cast<int>(direction_));
+    spdlog::debug("Value of time_diff: {}", time_diff);
+    spdlog::info("Exiting SwipeGestureEvent::End");
     return;
   }
 
@@ -26,9 +41,13 @@ void SwipeGestureEvent::End(libinput_event* event) {
       !key_codes.empty()) {
     InputInjector::Inject(key_codes);
   }
+
+  spdlog::info("Exiting SwipeGestureEvent::End");
 }
 
 void SwipeGestureEvent::Update(libinput_event* event) {
+  spdlog::info("Entering SwipeGestureEvent::Update");
+
   auto gesture_event = libinput_event_get_gesture_event(event);
   auto dx = libinput_event_gesture_get_dx_unaccelerated(gesture_event);
   auto dy = libinput_event_gesture_get_dy_unaccelerated(gesture_event);
@@ -37,14 +56,25 @@ void SwipeGestureEvent::Update(libinput_event* event) {
   sx_ += dx;
   sy_ += dy;
 
+  spdlog::debug("Value of sx_: {}", sx_);
+  spdlog::debug("Value of sy_: {}", sy_);
+
   if (std::abs(sx_) >= kThreshold) {
     // Handles horizontal swipes.
     time_ = time;
     direction_ =
       sx_ < 0 ? Gesture::Direction::kLeft : Gesture::Direction::kRight;
+
+    spdlog::info("Horizontal threshold met");
+    spdlog::debug("Value of direction_: {}", static_cast<int>(direction_));
   } else if (std::abs(sy_) >= kThreshold) {
     // Handles vertical swipes.
     time_ = time;
     direction_ = sy_ < 0 ? Gesture::Direction::kDown : Gesture::Direction::kUp;
+
+    spdlog::info("Vertical threshold met");
+    spdlog::debug("Value of direction_: {}", static_cast<int>(direction_));
   }
+
+  spdlog::info("Exiting SwipeGestureEvent::Update");
 }
