ceeb260d058c27c2a124c4324bbc44e16777f0bc
giosali <70548443+giosali@users.noreply.github.com>
2024-10-15 14:43:08 -0500

    Create CLI-invokable tasks from Makefile

---
+++ tasks.py
@@ -0,0 +1,32 @@
+from invoke import Context, Result, task
+
+NAME = "slither"
+BUILD_DIR = "build"
+TEST_BUILD_DIR = f"{BUILD_DIR}_test"
+
+
+@task
+def build(c: Context):
+    c.run(f"cmake -B {BUILD_DIR} -S . -G Ninja")
+    c.run(f"cmake --build {BUILD_DIR} -j 4")
+
+
+@task
+def clean(c: Context):
+    c.run(f"rm -rf ./{{{BUILD_DIR},{TEST_BUILD_DIR}}}")
+
+
+@task
+def run(c: Context):
+    result: Result = c.run(f"getcap ./{BUILD_DIR}/{NAME}")
+    if result.stdout == "":
+        c.run(f"sudo setcap cap_dac_override=ep ./{BUILD_DIR}/{NAME}")
+
+    c.run(f"./{BUILD_DIR}/{NAME} --verbose")
+
+
+@task
+def test(c: Context):
+    c.run(f"cmake -B {TEST_BUILD_DIR} -S ./test/")
+    c.run(f"cmake --build {TEST_BUILD_DIR}")
+    c.run(f"./{TEST_BUILD_DIR}/{NAME}_test")
