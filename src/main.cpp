9181c01f53a209b2e682411cfa0e0ac4ac1791c1
giosali <70548443+giosali@users.noreply.github.com>
2024-10-16 19:16:07 -0500

    Add command-line argument to activate GUI

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <gtkmm-4.0/gtkmm.h>
 #include <spdlog/spdlog.h>
 
 #include <exception>
@@ -6,16 +7,19 @@
 #include "argparse.hpp"
 #include "gesturesfile.h"
 #include "gesturewatcher.h"
+#include "ui/mainwindow.h"
 
 int main(int argc, char* argv[]) {
   auto program = argparse::ArgumentParser{"Slither"};
-
+  program.add_argument("--gui")
+    .help("Open the GUI to configure touchpad gestures")
+    .default_value(false)
+    .implicit_value(true);
   program.add_argument("--verbose")
     .help("Print various debugging information")
     .default_value(false)
     .implicit_value(true);
 
-  // Basic, just-in-case error handling for argument parsing.
   try {
     program.parse_args(argc, argv);
   } catch (const std::exception& e) {
@@ -24,6 +28,14 @@ int main(int argc, char* argv[]) {
     return 1;
   }
 
+  // GUI portion of the program.
+  auto gui = program.get<bool>("--gui");
+  if (gui) {
+    auto app = Gtk::Application::create("io.github.giosali.slither");
+    return app->make_window_and_run<MainWindow>(0, nullptr);
+  }
+
+  // Core portion of the program.
   auto verbose = program.get<bool>("--verbose");
   spdlog::set_level(verbose ? spdlog::level::debug : spdlog::level::off);
 
