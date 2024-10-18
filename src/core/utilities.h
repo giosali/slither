041a0bf07a11f878f50d3743b359d7d695245bff
giosali <70548443+giosali@users.noreply.github.com>
2024-10-18 15:24:58 -0500

    Add EnumRange utility function

--- src/core/utilities.h
+++ src/core/utilities.h
@@ -2,13 +2,20 @@
 #define UTILITIES_H
 
 #include <cstdint>
+#include <ranges>
 #include <string>
+#include <utility>
 #include <vector>
 
 class Utilities {
  public:
   Utilities() = delete;
 
+  static inline auto EnumRange = [](auto front, auto back) {
+    return std::views::iota(std::to_underlying(front),
+                            std::to_underlying(back) + 1) |
+           std::views::transform([](auto e) { return decltype(front)(e); });
+  };
   static std::string Join(const std::string& delimiter,
                           const std::vector<std::string>& elements);
   static std::string KeyCodeToString(uint32_t key_code);
