a8e728b26348952301eae981c687c8d380de2349
giosali <70548443+giosali@users.noreply.github.com>
2024-10-27 09:13:16 -0500

    Delete files

--- src/ui/gesturelistboxrow.cpp
+++
@@ -1,8 +0,0 @@
-#include "gesturelistboxrow.h"
-
-#include "gtkmm/listboxrow.h"
-
-GestureListBoxRow::GestureListBoxRow(const Gesture& gesture)
-    : Gtk::ListBoxRow{}, gesture_{gesture} {}
-
-Gesture GestureListBoxRow::GetGesture() const { return gesture_; }
