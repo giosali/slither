b18cece3358d6651b284b8d4a9a74dda70a397cd
giosali <70548443+giosali@users.noreply.github.com>
2024-10-28 00:39:20 -0500

    Remove directionstring file

--- src/ui/directionstring.cpp
+++
@@ -1,6 +0,0 @@
-#include "directionstring.h"
-
-DirectionString::DirectionString(Gesture::Direction direction)
-    : wxString{Gesture::StringifyDirection(direction)}, direction_{direction} {}
-
-Gesture::Direction DirectionString::GetDirection() const { return direction_; }
