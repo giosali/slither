ea40e709a6707c50cd4de409088f75c1e9ae441f
giosali <70548443+giosali@users.noreply.github.com>
2024-10-28 10:44:28 -0500

    Delete gesturestring files

--- src/ui/gesturestring.cpp
+++
@@ -1,6 +0,0 @@
-#include "gesturestring.h"
-
-GestureString::GestureString(const Gesture& gesture)
-    : wxString{gesture.ToString()}, gesture_{gesture} {}
-
-Gesture GestureString::GetGesture() const { return gesture_; }
